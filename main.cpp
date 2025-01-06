d74e4068bf966075abbd0cf28cdaae4e7b651d31
Ahsan Anjum <ahsananjum149@gmail.com>
2025-01-07 01:04:52 +0500

    Added Subtract Function to main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 using namespace std;
 
 void add(int a, int b);
+void sub(int a, int b);
 
 int main() {
     cout << "Hello World!";
     add(1, 2);
+    sub(2, 1);
     system("pause");
     return 0;
 }
 
 void add(int a, int b) {
     cout << a + b;
+}
+
+void sub(int a, int b) {
+    cout << a - b;
 }
