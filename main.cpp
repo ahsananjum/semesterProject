7c3ec33ac223436ec6bfafca54bc2ee3d3f6a0f3
Ahsan Anjum <ahsananjum149@gmail.com>
2025-01-07 00:46:28 +0500

    Added a new Add Function

--- main.cpp
+++ main.cpp
@@ -1,8 +1,14 @@
 #include<iostream>
 using namespace std;
 
+int add(int a, int b);
+
 int main() {
     cout << "Hello World!";
     system("pause");
     return 0;
+}
+
+int add(int a, int b) {
+    return a + b;
 }
